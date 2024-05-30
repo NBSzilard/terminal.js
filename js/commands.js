cf3116d2007975d1ca5693d600da07b0b5ac3cf1
N.B.SZ <84020193+NBSzilard@users.noreply.github.com>
2024-05-30 19:58:35 +0200

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,59 @@
+var youtube = "https://www.youtube.com/@asdasd35_";
+var github = "https://github.com/NBSzilard/";
+var password = "admin";
+
+test = [
+  "<br>",
+  "Hello world! 👋",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/@asdasd35_' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/NBSzilard' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">test</span>           Hello World!',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">restart</span>        Restart the terminal',
+  '<span class="command">echo</span>           Echo the input text',
+  '<span class="command">calc</span>           Calculate a mathematical expression',
+  "<br>",
+];
+
+
+banner = [
+  '<span class="index">© 2024 Terminal.js, All rights reserved.</span>',
+  "─╔╗───────────────╔╗",
+  "╔╝╚╗──────────────║║─╔╗",
+  "╚╗╔╬══╦═╦╗╔╦╦═╗╔══╣║─╚╬══╗",
+  "─║║║║═╣╔╣╚╝╠╣╔╗╣╔╗║║─╔╣══╣",
+  "─║╚╣║═╣║║║║║║║║║╔╗║╚╦╣╠══║",
+  "─╚═╩══╩╝╚╩╩╩╩╝╚╩╝╚╩═╩╣╠══╝",
+  "────────────────────╔╝║",
+  "────────────────────╚═╝",
+  '<span class="color2">Welcome to terminal.js web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
---
+++ js/main.js
@@ -0,0 +1,199 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("test@terminal.js:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  let args = cmd.split(' ');
+  let command = args[0];
+  let params = args.slice(1);
+
+  switch (command.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "test":
+      loopLines(test, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "sudo":
+      addLine("Oh no, you're not admin...", "color2", 80);
+      setTimeout(function() {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+      }, 1000);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "secret":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    // socials
+    case "youtube":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "github":
+    case "git":
+      addLine("Opening Github...", "color2", 80);
+      newTab(github);
+      break;
+    case "restart":
+      addLine("Restarting terminal...", "color2", 80);
+      setTimeout(() => {
+        location.reload()
+      }, 500);
+      break;
+    default:
+      if (params.length > 0) {
+        handleCommandWithArgs(command, params);
+      } else {
+        addLine("<span class=\"inherit\">Command not found or missing arguments. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      }
+      break;
+  }
+}
+
+function handleCommandWithArgs(command, params) {
+  switch (command.toLowerCase()) {
+    case "echo":
+      addLine(params.join(' '), "color2", 80);
+      break;
+    case "calc":
+      let result = calculate(params);
+      addLine("Result: " + result, "color2", 80);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function calculate(params) {
+  try {
+    let expression = params.join(' ');
+    let result = eval(expression);
+    return result;
+  } catch (e) {
+    return "Error in calculation";
+  }
+}
+
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
